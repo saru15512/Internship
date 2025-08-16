660ce5ea5fa416632dcc267e4c74090f240fa193
saru15512 <sarumathir2004@gmail.com>
2025-08-16 15:25:28 +0530

    second version

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './GetStarted.css'
+const GetStarted = () => {
+  return (
+    <section className="q-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter innerContainer">
+                <span className='primaryText'>Get Started With Homyz</span>
+                <span className='secondaryText'>Subscribe and find super attractive price quotes from us.
+                    <br/>Find your residence soon
+                </span>
+                <button className='button'>
+                    <a href="mailto:saruramesh2004@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default GetStarted
